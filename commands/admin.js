6357832fc480108df2298947b0d2810ca4fe7127
Jakub Mańczak <56172798+jakubmanczak@users.noreply.github.com>
2020-12-12 22:07:59 +0100

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -38,11 +38,11 @@ module.exports = {
         case 'botAnnouncement':{
           const botAnnouncementEmbed = new Discord.MessageEmbed()
             .setTitle('Hej,')
-            .setDescription('W ciągu ostatnich trzech dni zostałem stworzony ja - bot Gractwa')
+            .setDescription('W ciągu ostatnich trzech dni zostałem stworzony ja - bot Gractwa.')
             .setColor(config.embedColor)
             .addField('Moja funkcja','Zastąpiłem wiadomości na kanałach `#witaj` i `#regulamin`, serwisuję również `#rolki`.')
             .addField('Zmiany',`Wraz z moim przybyciem zostało uproszczone nasze logo, w regulaminie czekają na Was dwie nowe zasady (niezbyt restrykcyjne ;3), forma serwera została uproszczona.`)
-            .addField('Rolki? Co?', `Rolki To!\n\nMogą one być wykorzystywane do zbierania osób do grania bądź do rozmów. Było to coś o co poproszone naszą administrację już dawno temu. Dziś jest to już dla Was dostępne!`)
+            .addField('Rolki? Co?', `Rolki To!\n\nMogą one być wykorzystywane do zbierania osób do grania/rozmów. Było to coś o co poproszono naszą administrację już dawno temu. Dziś jest to już dla Was dostępne!`)
             .attachFiles('./gractwo.png')
             .setFooter(`by j4mesen`,'attachment://gractwo.png')
             .setTimestamp();
