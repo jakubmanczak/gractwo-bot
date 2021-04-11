3cb3d4d97f2951f7c757a7ba1d08b2bc6a0f17d3
Jakub Mańczak <56172798+jakubmanczak@users.noreply.github.com>
2021-04-11 12:17:07 +0200

    +jmd.js

---
+++ commands/jmd.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: 'jmd',
+  description: 'Mówi jemenikowi żeby zamknął dupę (na vc).',
+  args: false,
+  guildOnly: true,
+	execute(msg, args, Discord, config, client) {
+    msg.delete()
+      .catch(console.error);
+    const jmdEmbed = new Discord.MessageEmbed()
+      .setTitle('chotto matte')
+      .setColor(config.embedColor)
+      .setDescription(`Mówienie jemenikowi żeby zamknął dupę...`)
+      .attachFiles('./gractwo.png')
+      .setFooter(`Komenda wywołana przez ${msg.author.username}`, 'attachment://gractwo.png')
+      .setTimestamp();
+    msg.channel.send(jmdEmbed);
+    console.log(`Responded to jmd request by ${msg.author.username}`);
+	},
+};
