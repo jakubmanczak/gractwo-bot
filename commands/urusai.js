d6bb3fb4f21d9bd56880ef32fa039f8234609ea5
Jakub Mańczak <56172798+jakubmanczak@users.noreply.github.com>
2021-04-11 12:21:08 +0200

    jzd => urusai

--- commands/urusai.js
+++ commands/urusai.js
@@ -1,19 +1,19 @@
 module.exports = {
-	name: 'jmd',
+	name: 'urusai',
   description: 'Mówi jemenikowi żeby zamknął dupę (na vc).',
   args: false,
   guildOnly: true,
 	execute(msg, args, Discord, config, client) {
     msg.delete()
       .catch(console.error);
-    const jmdEmbed = new Discord.MessageEmbed()
+    const jzdEmbed = new Discord.MessageEmbed()
       .setTitle('Chotto matte! >w<')
       .setColor(config.embedColor)
       .setDescription(`Mówienie jemenikowi żeby zamknął dupę...`)
       .attachFiles('./gractwo.png')
       .setFooter(`Komenda wywołana przez ${msg.author.username}`, 'attachment://gractwo.png')
       .setTimestamp();
-    msg.channel.send(jmdEmbed);
-    console.log(`Responded to jmd request by ${msg.author.username}`);
+    msg.channel.send(jzdEmbed);
+    console.log(`Responded to urusai request by ${msg.author.username}`);
 	},
 };
